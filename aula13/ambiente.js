0eaf9f9fb33e9ff81deaff6b7594e0da64ee0c7e
fflaviotito <fflavio.tito@gmail.com>
2023-12-12 15:49:42 -0300

    aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+// Essa é uma estrutura de repetição com teste lógico no início
+var contador = 1
+while (contador <= 3) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+
+// Essa é uma estrutura de repetição com teste lógico no final
+var c = 1
+do {
+    console.log(`Esse foi o ${c}`)
+    c++
+} while (c <= 3)
