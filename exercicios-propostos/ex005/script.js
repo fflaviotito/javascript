a9da9ea77a37c5fcc17b554c32ec56c76e24df9c
fflaviotito <fflavio.tito@gmail.com>
2023-12-13 14:24:02 -0300

    ex05 proposto

--- exercicios-propostos/ex005/script.js
+++ exercicios-propostos/ex005/script.js
@@ -1,12 +1,16 @@
+var num = document.getElementById('num')
+var lista = document.getElementById('lista')
+var res = document.getElementById('res')
+
+var dados = []
+
 function adicionar() {
-    var num = document.getElementById('num').value
-    if (num < 1 || num > 100) {
-        alert('Número inválido!')
+    if (num.value < 1 || num.value > 100) {
+        alert('Número inválido')
+    } else if (dados.indexOf(num.value) != -1) {
+        alert('O número já consta na lista')
     } else {
-        var lista = document.getElementById('lista')
-        var item = document.createElement('option')
-        item.text = `Valor ${num} adicionado.`
-        lista.appendChild(item)
+
     }
 }
 
