ac19f8e76b037c8f24e7bc5fe642f89c5eca0211
fflaviotito <fflavio.tito@gmail.com>
2023-12-13 14:35:36 -0300

    Update script.js

--- exercicios-propostos/ex005/script.js
+++ exercicios-propostos/ex005/script.js
@@ -2,7 +2,8 @@ var num = document.getElementById('num')
 var lista = document.getElementById('lista')
 var res = document.getElementById('res')
 
-var dados = []
+var dados = [4]
+dados.push(num)
 
 function adicionar() {
     if (num.value < 1 || num.value > 100) {
@@ -10,7 +11,10 @@ function adicionar() {
     } else if (dados.indexOf(num.value) != -1) {
         alert('O número já consta na lista')
     } else {
-
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
     }
 }
+
 
