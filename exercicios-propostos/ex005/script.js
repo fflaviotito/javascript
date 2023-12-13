b7141d56996cd0a687e1d959840fd3d72fd64033
fflaviotito <fflavio.tito@gmail.com>
2023-12-13 15:17:56 -0300

    Update script.js

--- exercicios-propostos/ex005/script.js
+++ exercicios-propostos/ex005/script.js
@@ -2,18 +2,49 @@ var num = document.getElementById('num')
 var lista = document.getElementById('lista')
 var res = document.getElementById('res')
 
-var dados = [4]
-dados.push(num)
+var dados = []
 
 function adicionar() {
     if (num.value < 1 || num.value > 100) {
         alert('Número inválido')
-    } else if (dados.indexOf(num.value) != -1) {
+    } else if (dados.indexOf(Number(num.value)) != -1) {
         alert('O número já consta na lista')
     } else {
+        dados.push(Number(num.value))
         var item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        res.innerHTML = ''
+    }
+    num.value = ""
+    num.focus()
+}
+
+function finalizar() {
+    if (dados.length == 0){
+        alert('Adicione valores antes de finalizar')
+    } else {
+        var total = dados.length
+        res.innerHTML = `<p>Ao todo, temos ${total} números cadastrados</p>`
+
+        var maior = dados[0]
+        var menor = dados[0]
+        var soma = 0
+        for (var pos in dados) {
+            soma += dados[pos]
+            if (dados[pos] > maior) {
+                maior = dados[pos]
+            }
+            if (dados[pos] < menor) {
+                menor = dados[pos]
+            }
+        }
+        res.innerHTML += `<p>O maior valor informado é ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado é ${menor}</p>`
+        res.innerHTML += `<p>A soma dos valores informados é ${soma}</p>`
+
+        var media = soma / total
+        res.innerHTML += `<p>A média dos valores informados é ${media}</p>`
     }
 }
 
